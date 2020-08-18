dab7a9bd29ff72b57b2c37a82c02ef410443b2ff
username <578173534@qq.com>
2020-08-18 10:47:14 +0800

    Spring代码

--- ch01-hello-spring/src/main/java/com/ggs/service/impl/SomeServiceImpl.java
+++ ch01-hello-spring/src/main/java/com/ggs/service/impl/SomeServiceImpl.java
@@ -3,6 +3,11 @@ package com.ggs.service.impl;
 import com.ggs.service.SomeService;
 
 public class SomeServiceImpl implements SomeService {
+
+    public SomeServiceImpl() {
+        System.out.println("SomeServiceImpl的无参数构造方法");
+    }
+
     @Override
     public void doSome() {
         System.out.println("执行了SomeServiceImpl的doSome()方法");
---
+++ ch02-di-xml/src/main/java/com/ggs/ba01/Student.java
@@ -0,0 +1,46 @@
+package com.ggs.ba01;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public Student() {
+        System.out.println("spring会调用类的无参数构造方法创建对象");
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /*
+         需要有set方法，没有set方法是报错的。
+         Bean property 'name' is not writable or has an invalid setter method
+    */
+    public void setName(String name) {
+        System.out.println("setName:" + name);
+        this.name = name.toUpperCase();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setAge:" + age);
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
