d03a2661f6bf14aac52a90b187a18da1290738e0
xtremely <578173534@qq.com>
2020-09-06 12:43:54 +0800

    Spring代码

--- ch11-spring-web/src/main/java/com/ggs/controller/RegisterServlet.java
+++ ch11-spring-web/src/main/java/com/ggs/controller/RegisterServlet.java
@@ -2,9 +2,10 @@ package com.ggs.controller;
 
 import com.ggs.domain.Student;
 import com.ggs.service.StudentService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,16 +14,32 @@ import java.io.IOException;
 
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         request.setCharacterEncoding("utf-8");
+
         String strId = request.getParameter("id");
         String strName = request.getParameter("name");
         String strEmail = request.getParameter("email");
         String strAge = request.getParameter("age");
 
         //创建spring的容器对象
-        String config = "applicationContext.xml";
-        ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
+        //String config = "spring.xml";
+        //ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
+
+        WebApplicationContext ctx = null;
+
+        //获取ServletContext中的容器对象，创建好的容器对象，拿来就用
+        /*String key = WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE;
+        Object attr = getServletContext().getAttribute(key);
+        if (attr != null) {
+            ctx = (WebApplicationContext) attr;
+        }*/
+
+        //使用框架中的方法，获取容器对象
+        ServletContext sc = getServletContext();
+        ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(sc);
         System.out.println("容器对象的信息====" + ctx);
+
         //获取service
         StudentService service = (StudentService) ctx.getBean("studentService");
         Student student = new Student(Integer.valueOf(strId), strName, strEmail, Integer.valueOf(strAge));
